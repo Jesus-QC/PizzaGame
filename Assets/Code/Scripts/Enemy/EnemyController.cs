c6b56547eab6688e8d39f090dc93bb34b547b053
Jesus QC <69375249+Jesus-QC@users.noreply.github.com>
2025-11-02 20:05:06 +0100

    Enemy sound effects

--- Assets/Code/Scripts/Enemy/EnemyController.cs
+++ Assets/Code/Scripts/Enemy/EnemyController.cs
@@ -4,11 +4,56 @@ namespace Code.Scripts.Enemy
 {
     public class EnemyController : MonoBehaviour
     {
+        private const float MinStingerInterval = 10f;
+
         public static EnemyController Instance { get; private set; }
 
+        public AudioSource EffectsSource;
+        public AudioClip StingerEffect;
+
+        private bool _seenByPlayer;
+        private float _timeSinceLastSeen = MinStingerInterval;
+        
+        public bool IsBeingSeen
+        {
+            get => _seenByPlayer;
+            set
+            {
+                if (value == _seenByPlayer)
+                    return;
+
+                _seenByPlayer = value;
+
+                if (value)
+                {
+                    EffectsSource.volume = 1;
+                    if (_timeSinceLastSeen > MinStingerInterval)
+                    {
+                        EffectsSource.PlayDelayed(5);
+                        EffectsSource.PlayOneShot(StingerEffect);
+                    }
+                }
+
+                _timeSinceLastSeen = 0f;
+            }
+        }
+        
         private void Awake()
         {
             Instance = this;
+        }
+
+        void Update()
+        {
+            _timeSinceLastSeen += Time.deltaTime;
+
+            if (_seenByPlayer || !EffectsSource.isPlaying)
+                return;
+            
+            EffectsSource.volume = Mathf.Max(0, EffectsSource.volume - Time.deltaTime * 0.1f);
+
+            if (EffectsSource.volume <= 0)
+                EffectsSource.Stop();
         }
     }
 }
--- Assets/Code/Scripts/Player/PlayerController.cs
+++ Assets/Code/Scripts/Player/PlayerController.cs
@@ -5,7 +5,8 @@ namespace Assets.Code.Scripts.Player
     public class PlayerController : MonoBehaviour
     {
         public static PlayerController Instance { get; private set; }
-        
+ 
+        public AudioSource GlobalAudioSource;
         public Rigidbody PlayerRigidbody;
         public MovementController MovementController;
         public CameraController CameraController;
