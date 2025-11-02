870749487fa76b762a5d110a3eea9f74a10bfd02
anwei <anwei2004925@gmail.com>
2025-11-02 19:23:58 +0100

    add climbing sound

--- Assets/Code/Scripts/Player/LadderController.cs
+++ Assets/Code/Scripts/Player/LadderController.cs
@@ -6,6 +6,8 @@ public class LadderController : MonoBehaviour
 {
     bool isClimbing = false;
     Transform ladder;
+    public AudioSource audioSource;
+    public AudioClip clip;
 
     void OnTriggerEnter(Collider other)
     {
@@ -31,6 +33,21 @@ public class LadderController : MonoBehaviour
         {
             float verticalInput = Keyboard.current.wKey.isPressed ? 1f : Keyboard.current.sKey.isPressed? -1f : 0f;
             transform.position += verticalInput * Time.deltaTime * ladder.forward;
+            
+            if (verticalInput != 0f)
+            {
+                if (!audioSource.isPlaying)
+                {
+                    audioSource.clip = clip;
+                    audioSource.loop = true;
+                    audioSource.Play();
+                }
+            }
+            else if (audioSource.isPlaying)
+            {
+                audioSource.Stop();
+            }
+            
             if (Keyboard.current.sKey.isPressed && Physics.Raycast(transform.position, Vector3.down, 0.2f))
                 ExitLadder();
         }
@@ -42,6 +59,6 @@ public class LadderController : MonoBehaviour
         ladder = null;
         GetComponent<Rigidbody>().useGravity = true;
         GetComponent<MovementController>().enabled = true;
-        
+        audioSource.Stop();
     }
 }
