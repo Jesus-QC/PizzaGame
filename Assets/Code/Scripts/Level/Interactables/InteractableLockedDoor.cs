167eb540a4419a0f924df05992303ee065be79c3
jennyyyzz <jenny.zhu@alumnos.upm.es>
2025-11-20 22:32:40 +0100

    Locked door

---
+++ Assets/Code/Scripts/Level/Interactables/InteractableLockedDoor.cs
@@ -0,0 +1,80 @@
+﻿using Assets.Code.Scripts.Player;
+using UnityEngine;
+
+namespace Code.Scripts.Level.Interactables
+{
+    public class InteractableLockedDoor : MonoBehaviour, IInteractable
+    {
+        private const float CooldownTime = 0.5f;
+        
+        private static readonly int OpenAnimation = Animator.StringToHash("Open");
+        
+        public AudioSource AudioSource;
+        public AudioClip OpenClip, CloseClip;
+        public Animator DoorAnimator;
+        public GameObject Key;
+        private bool _isOpen;
+        private bool _isLocked;
+        private float _lastInteractionTime;
+
+        public bool IsOpen
+        {
+            get => _isOpen;
+            set
+            {
+                _isOpen = value;
+                if (_isOpen) 
+                    Open();
+                else 
+                    Close();
+                
+                DoorAnimator.SetBool(OpenAnimation, _isOpen);
+                _lastInteractionTime = Time.time;
+            }
+        }
+
+        public bool IsLocked
+        {
+            get => _isLocked;
+            set
+            {
+                _isLocked = value;
+            }
+        }
+
+        public void Start ()
+        {
+            IsLocked = true;
+        }
+
+        public void Interact()
+        {
+            if (Time.time - _lastInteractionTime < CooldownTime)
+                return;
+
+            var held = PlayerController.Instance.ItemsController.HeldObject;
+
+            if (_isLocked)
+            {
+                if (held.gameObject != Key)
+                {
+                    Debug.Log("Door is locked! You need the correct key in hand.");
+                    return;
+                }
+                _isLocked = false;
+            }
+
+            IsOpen = !IsOpen;
+        }
+
+        private void Open()
+        {
+            AudioSource.PlayOneShot(OpenClip);
+        }
+
+        private void Close()
+        {
+            AudioSource.PlayOneShot(CloseClip);
+        }
+    }
+}
