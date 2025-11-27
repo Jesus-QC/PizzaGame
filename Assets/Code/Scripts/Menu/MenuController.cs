6fdd9623408c1710dd0924d4f0f9a4d7169947d8
Jesus-QC <69375249+Jesus-QC@users.noreply.github.com>
2025-11-27 17:30:59 +0100

    Menu

---
+++ Assets/Code/Scripts/Menu/MenuController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace Code.Scripts.Menu
+{
+    public class MenuController : MonoBehaviour
+    {
+        public AnimationController AnimationController;
+
+        public void StartGame()
+        {
+            
+        }
+    }
+}
