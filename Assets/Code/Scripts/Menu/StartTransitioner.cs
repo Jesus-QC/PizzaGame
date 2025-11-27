9154405dd55ef2fe7e38d9c728e650200f469f37
Jesus-QC <69375249+Jesus-QC@users.noreply.github.com>
2025-11-27 18:21:50 +0100

    Add music fade-out functionality and update menu scene settings

--- Assets/Code/Scripts/Menu/StartTransitioner.cs
+++ Assets/Code/Scripts/Menu/StartTransitioner.cs
@@ -8,6 +8,7 @@ namespace Code.Scripts.Menu
         public static StartTransitioner Instance;
 
         public Animator TransitionAnimator;
+        public AudioSource MenuMusic;
 
         private void Awake()
         {
@@ -23,8 +24,19 @@ namespace Code.Scripts.Menu
 
         private IEnumerator TransitionCoroutine(string sceneName)
         {
+            StartCoroutine(FadeOutMusic());
             yield return new WaitForSeconds(1f);
             UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+        }
+
+        private IEnumerator FadeOutMusic()
+        {
+            float startVolume = MenuMusic.volume;
+            for (float t = 0; t < 1f; t += Time.deltaTime)
+            {
+                MenuMusic.volume = Mathf.Lerp(startVolume, 0, t / 1f);
+                yield return null;
+            }
         }
     }
 }
