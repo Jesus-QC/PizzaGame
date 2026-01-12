8fc5c04b293bd595280104c4a011170d4c674dda
Jesus-QC <69375249+Jesus-QC@users.noreply.github.com>
2026-01-12 21:29:25 +0100

    Fini

--- Assets/Code/Scripts/Menu/MenuController.cs
+++ Assets/Code/Scripts/Menu/MenuController.cs
@@ -4,6 +4,12 @@ namespace Code.Scripts.Menu
 {
     public class MenuController : MonoBehaviour
     {
+        void Start()
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
         public void StartGame()
         {
             StartTransitioner.Instance.StartTransitionToScene("Cooking");
